e424498a258dfd52d5cc59cf2ef4794e2fc91e76
ARJUN TELI <127530301+arjunteli@users.noreply.github.com>
2023-10-04 02:54:31 +0530

    Update and rename incomeTaxCalculate.java to IncomeTaxCalculate.java

--- IncomeTaxCalculate.java
+++ IncomeTaxCalculate.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class incomeTaxCalculate {
+public class IncomeTaxCalculate {
     public static void main(String[]args){
         Scanner sc= new Scanner(System.in);
         //input income
