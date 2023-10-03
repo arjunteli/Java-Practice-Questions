2f00d97652341cd86a234a9e8240636fdb51e26f
ARJUN TELI <127530301+arjunteli@users.noreply.github.com>
2023-10-04 02:55:36 +0530

    Update and rename checkNumberIsPositiveORNegative.java to CheckNumberIsPositiveORNegative.java

--- CheckNumberIsPositiveORNegative.java
+++ CheckNumberIsPositiveORNegative.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class checkNumberIsPositiveORNegative {
+public class CheckNumberIsPositiveORNegative {
     public static void main(String[] args) {
         Scanner sc =new Scanner (System.in);
 
