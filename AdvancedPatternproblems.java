7f7b45c88ad19701b0d78d81d80fdb79920da88a
ARJUN TELI <127530301+arjunteli@users.noreply.github.com>
2023-10-04 02:52:11 +0530

    Update and rename advancedPatternproblems.java to AdvancedPatternproblems.java

--- AdvancedPatternproblems.java
+++ AdvancedPatternproblems.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class advancedPatternproblems {
+public class AdvancedPatternproblems {
     public static void hollowRectangle(int m, int n){
         for(int i=0;i<m;i++){
             for(int j =0;j<n;j++){
