5bce4876a9ec7451f4a8dc7d52bbc746a67453af
ARJUN TELI <modiarjunsahu1161999@gmail.com>
2023-11-03 12:24:45 +0530

    Update and rename areaOfCircle.java to AreaOfCircle.java

--- AreaOfCircle.java
+++ AreaOfCircle.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class areaOfCircle {
+public class AreaOfCircle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float r = sc.nextFloat();
